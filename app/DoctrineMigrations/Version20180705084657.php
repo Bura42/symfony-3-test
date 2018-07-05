eb474d611499990aab8ee2522b94ead3c19544d6
m.horiachev <m.horiachev@levi9.com>
2018-07-05 12:20:38 +0300

    before form

---
+++ app/DoctrineMigrations/Version20180705084657.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180705084657 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE genus ADD CONSTRAINT FK_38C5106E71179CD6 FOREIGN KEY (name_id) REFERENCES sub_family (id)');
+        $this->addSql('CREATE INDEX IDX_38C5106E71179CD6 ON genus (name_id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE genus DROP FOREIGN KEY FK_38C5106E71179CD6');
+        $this->addSql('DROP INDEX IDX_38C5106E71179CD6 ON genus');
+    }
+}
---
+++ app/DoctrineMigrations/Version20180705085547.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180705085547 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE genus CHANGE name_id sub_family_id INT NOT NULL, CHANGE sub_family name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE genus ADD CONSTRAINT FK_38C5106ED15310D4 FOREIGN KEY (sub_family_id) REFERENCES sub_family (id)');
+        $this->addSql('CREATE INDEX IDX_38C5106ED15310D4 ON genus (sub_family_id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE genus DROP FOREIGN KEY FK_38C5106ED15310D4');
+        $this->addSql('DROP INDEX IDX_38C5106ED15310D4 ON genus');
+        $this->addSql('ALTER TABLE genus CHANGE name sub_family VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE sub_family_id name_id INT NOT NULL');
+    }
+}
